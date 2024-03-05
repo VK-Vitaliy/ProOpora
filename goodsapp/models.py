f959f1fde9e07b3b731afe7e51270068fbbaba7e
Vitaliy <kulvitall.vk@gmail.com>
2024-03-05 15:27:48 +0300

    added pagination

--- goodsapp/models.py
+++ goodsapp/models.py
@@ -28,6 +28,7 @@ class Product(models.Model):
         db_table = 'product'
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
+        ordering = ("id", )
 
     def __str__(self):
         return self.name
--- goodsapp/views.py
+++ goodsapp/views.py
@@ -1,13 +1,22 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_list_or_404
 
 from goodsapp.models import Product
 
 
 def catalog(request, category_slug):
-    goods = get_list_or_404(Product.objects.filter(category__slug=category_slug))
+    if category_slug == 'all':
+        goods = Product.objects.all()
+    else:
+        goods = get_list_or_404(Product.objects.filter(category__slug=category_slug))
+
+    page = request.GET.get('page', 1)
+    paginator = Paginator(goods, per_page=6)
+    current_page = paginator.page(int(page))
     context = {
         'title': 'ProOpora - catalog',
-        'goods': goods
+        'goods': current_page,
+        'slug_url': category_slug
     }
     return render(request, 'goodsapp/catalog.html', context)
 
