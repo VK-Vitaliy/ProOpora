61990398acdc6334fe94f82010f58d77b1174fd6
Vitaliy <kulvitall.vk@gmail.com>
2024-03-05 16:51:21 +0300

    added product filters and urlencode

--- goodsapp/views.py
+++ goodsapp/views.py
@@ -5,12 +5,21 @@ from goodsapp.models import Product
 
 
 def catalog(request, category_slug):
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
+    page = request.GET.get('page', 1)
+
     if category_slug == 'all':
         goods = Product.objects.all()
     else:
         goods = get_list_or_404(Product.objects.filter(category__slug=category_slug))
 
-    page = request.GET.get('page', 1)
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by != 'default':
+        goods = goods.order_by(order_by)
+
     paginator = Paginator(goods, per_page=6)
     current_page = paginator.page(int(page))
     context = {
